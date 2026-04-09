53cf137e3acf91b308e62e97b806af6cf6f9a202
Agustin Decuadro <adecuadro@accesa.com.uy>
2026-04-09 00:14:55 -0300

    Chore/project setup (#2)
    * chore: setup eslint prettier typescript and husky
    
    * chore: config tailwindcss
    
    * chore: setup github actions ci and deploy workflows
    
    * chore: implement base for github pages

--- eslint.config.js
+++ eslint.config.js
@@ -2,8 +2,11 @@ import js from '@eslint/js'
 import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
+import reactPlugin from 'eslint-plugin-react'
 import tseslint from 'typescript-eslint'
+import prettier from 'eslint-config-prettier'
 import { defineConfig, globalIgnores } from 'eslint/config'
+import { version } from 'os'
 
 export default defineConfig([
   globalIgnores(['dist']),
@@ -14,10 +17,24 @@ export default defineConfig([
       tseslint.configs.recommended,
       reactHooks.configs.flat.recommended,
       reactRefresh.configs.vite,
+      reactPlugin.configs.flat.recommended,
+      reactPlugin.configs.flat['jsx-runtime'],
+      prettier,
     ],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
+    },
+    rules: {
+      '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+      '@typescript-eslint/no-explicit-any': 'error',
+      'react/self-closing-comp': 'warn',
+      'react/jsx-curly-brace-presence': ['warn', { props: 'never', children: 'never' }],
+    },
+    settings: {
+      react: {
+        version: '19.2.4',
+      },
     },
   },
 ])
