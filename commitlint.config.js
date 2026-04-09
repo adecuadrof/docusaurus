7f8473bf551ecbc97ac67fac3f75f128397b06d4
Agustin Decuadro <adecuadro@accesa.com.uy>
2026-04-09 00:01:41 -0300

    Chore/project setup (#1)
    * chore: setup eslint prettier typescript and husky
    
    * chore: config tailwindcss
    
    * chore: setup github actions ci and deploy workflows

---
+++ commitlint.config.js
@@ -0,0 +1,13 @@
+export default {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [
+      2,
+      'always',
+      ['feat', 'fix', 'chore', 'docs', 'refactor', 'test', 'style', 'hotfix'],
+    ],
+    'subject-case': [2, 'always', 'lower-case'],
+    'subject-empty': [2, 'never'],
+    'type-empty': [2, 'never'],
+  },
+}
